d0907722f3a0546613fe94983e5d1e88156b7693
lehongson1998 <lhongson2909@gmail.com>
2022-08-30 09:36:51 +0700

    up lại

--- 08_validation/exercise/singup-form/src/main/java/com/singup_form/model/Customer.java
+++ 08_validation/exercise/singup-form/src/main/java/com/singup_form/model/Customer.java
@@ -1,2 +1,30 @@
-package com.singup_form.model;public class Customer {
+package com.singup_form.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+public class Customer extends Person{
+    private boolean gender;
+
+    public Customer(Long id, String name, boolean gender) {
+        super(id, name);
+        this.gender = gender;
+    }
+
+    public Customer(String name, boolean gender) {
+        super(name);
+        this.gender = gender;
+    }
+
+    public Customer() {
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
 }
--- 08_validation/exercise/singup-form/src/main/java/com/singup_form/model/Person.java
+++ 08_validation/exercise/singup-form/src/main/java/com/singup_form/model/Person.java
@@ -1,2 +1,39 @@
-package com.singup_form.model;public class Person {
+package com.singup_form.model;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Person(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
