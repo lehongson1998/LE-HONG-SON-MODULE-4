d801fc054e37e7d83524a4a5c74377e098f8cbe9
lehongson1998 <lhongson2909@gmail.com>
2022-09-01 06:17:45 +0700

    up bt 11 nà

---
+++ 06_jpa/exercise/blog-managements/src/test/java/com/blog_managements/BlogManagementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog_managements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogManagementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
